1eba6bf8838b6671594c89670b68229f51248ead
Santosh <santoshkumar23kky@gmail.com>
2023-09-17 08:07:09 +0530

    lms Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='flex justify-center items-center text-4xl font-bold'>Hello Santosh</h1>
+    </div>
+  )
+}
+
+export default App;
